dacfe0814cb2921ed8b48b36bf2435e8dfb85632
Michael Rajchert <mikerajchert@gmail.com>
2018-12-07 01:22:05 -0500

    Bot Created

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = "Lol-you-thought-you-could-get-clever-here!";
+
+bot.on('message', function(message){
+    if(message.content == 'Hello'){
+        message.reply('NCG REPPPP AYE BEEP BOOP BOI');
+    }
+});
+
+bot.on('ready',function(){
+    console.log('Started');
+});
+
+bot.login(TOKEN);
